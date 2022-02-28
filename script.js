7568c2f3409b2377b9059c761cab8acbb338e46c
Carlos William <carloswtmachado@gmail.com>
2022-02-28 17:39:11 -0400

    Ultimos pedidos pegos da api

--- script.js
+++ script.js
@@ -1,9 +1,38 @@
 function selectOption(selected) {
-    selected.children[0].style.border = "3px solid #404EED"
-    let figures = selected.parentElement.children
+    let figures = selected.parentElement.children;
     for (let i of figures) {
-        if (i != selected) {
-            i.children[0].style.border = "0.1px solid #C4C4C4"
-        }
+        if (i != selected) i.children[0].classList.remove('selected');
+        else i.children[0].classList.add('selected');
     }
 }
+
+const TemplateCardPedido = (item) => {
+    return `
+    <figure class="pedidos-fig">
+        <img src="${item.image}" alt="Blusa ${item.id}" width="180px" height="180px">
+        <figcaption class="roboto-text">
+            Criador: ${item.owner}
+        </figcaption>
+    </figure>
+  `
+}
+
+const queryGetApi = async() => {
+    const queryUrl = await fetch(`https://mock-api.driven.com.br/api/v4/shirts-api/shirts`);
+    const queryResponseJson = await queryUrl.json();
+    return queryResponseJson;
+}
+
+const selectElement = (element, type) => {
+    return (type === 'all') ? document.querySelectorAll(element) : document.querySelector(element);
+}
+
+const renderCardsScreen = async() => {
+    const pedidosContainer = selectElement('.ultimos-pedidos > div', 'single');
+    const pedidos = await queryGetApi();
+    pedidos.forEach(item => {
+        pedidosContainer.innerHTML += TemplateCardPedido(item);
+    })
+}
+
+renderCardsScreen();
