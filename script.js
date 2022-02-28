c815c6bc07308558bfaa876db4351eca2a56e7c9
Carlos William <carloswtmachado@gmail.com>
2022-02-28 19:18:13 -0400

    selecionar obrigatoriamente e somente 1 tipo de modelo, gola e tecido, preencher obrigatoriamente um campo de texto para colocar um link

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 function selectOption(selected) {
     let figures = selected.parentElement.children;
-    for (let i of figures) {
-        if (i != selected) i.children[0].classList.remove('selected');
-        else i.children[0].classList.add('selected');
+    for (let figure of figures) {
+        if (figure != selected) figure.children[0].classList.remove('selected');
+        else figure.children[0].classList.add('selected');
     }
+    changeFunction(document.querySelector('.img-link').value);
 }
 
 const TemplateCardPedido = (item) => {
@@ -11,7 +12,7 @@ const TemplateCardPedido = (item) => {
     <figure class="pedidos-fig">
         <img src="${item.image}" alt="Blusa ${item.id}" width="180px" height="180px">
         <figcaption class="roboto-text">
-            Criador: ${item.owner}
+            <b>Criador:</b> ${item.owner}
         </figcaption>
     </figure>
   `
@@ -28,11 +29,38 @@ const selectElement = (element, type) => {
 }
 
 const renderCardsScreen = async() => {
-    const pedidosContainer = selectElement('.ultimos-pedidos > div', 'single');
+    const pedidosContainer = document.querySelector('.ultimos-pedidos > div');
     const pedidos = await queryGetApi();
     pedidos.forEach(item => {
         pedidosContainer.innerHTML += TemplateCardPedido(item);
     })
 }
 
+function changeFunction(value) {
+    const modelos = document.querySelector('.modelo').children;
+    let flagM = false;
+    const golas = document.querySelector('.gola').children;
+    let flagG = false;
+    const tecidos = document.querySelector('.tecido').children;
+    let flagT = false;
+    for (let modelo of modelos)
+        for (let classe of modelo.children[0].classList)
+            if (classe == 'selected') flagM = true;
+    for (let gola of golas)
+        for (let classe of gola.children[0].classList)
+            if (classe == 'selected') flagG = true;
+    for (let tecido of tecidos)
+        for (let classe of tecido.children[0].classList)
+            if (classe == 'selected') flagT = true;
+    const reg = /^http(s?):\/\/.*/;
+    const button = document.querySelector('.img-submit');
+    let ok = reg.exec(value);
+    if (ok && flagM && flagG && flagT) {
+        button.disabled = false;
+        button.classList.add('enabled');
+    } else {
+        button.disabled = true;
+        button.classList.remove('enabled');
+    }
+}
 renderCardsScreen();
